5fe8066c82bb5e64f5439ec31f11512cd1d6207d
Shivalik-capital <sorendatta7@gmail.com>
2026-09-06 11:21:31 +0530

    chore: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito, Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -62,6 +63,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
         <main id="main-content" className="flex-1">
           {children}
         </main>
+        <Analytics />
         <Footer />
       </body>
     </html>
